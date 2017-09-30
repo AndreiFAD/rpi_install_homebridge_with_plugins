cafb0d40dc00a03b3210f8ab30ac8107fe125b3b
AndreiFAD <andrei.dumitru.fechete@gmail.com>
2017-09-30 19:52:10 +0200

    upd

--- install.sh
+++ install.sh
@@ -18,8 +18,6 @@ sudo apt-get install -y git make
 sudo apt-get install -y git-core libnss-mdns libavahi-compat-libdnssd-dev
 echo "downloads node v8.5 files..."
 echo ""
-echo ""
-echo ""
 wget https://nodejs.org/dist/v8.5.0/node-v8.5.0-linux-armv6l.tar.xz
 tar xJvf node-v8.5.0-linux-armv6l.tar.xz
 sudo mkdir -p /opt/node
@@ -27,16 +25,12 @@ sudo mv node-v8.5.0-linux-armv6l/* /opt/node/
 sudo update-alternatives --install "/usr/bin/node" "node" "/opt/node/bin/node" 1
 sudo update-alternatives --install "/usr/bin/npm" "npm" "/opt/node/bin/npm" 1
 echo ""
-echo ""
-echo ""
 sudo apt-get install nodejs -y
 sudo npm install -g node-gyp
 sudo npm install -g n
 sudo n lts
 sudo apt-get install build-essential -y
 sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp
-echo ""
-echo ""
 echo ""
 echo "install packege for plugins"
 sudo apt-get install libtool git build-essential autoconf automake -y 
@@ -50,8 +44,6 @@ sudo make
 sudo make install
 cd /home/pi
 echo ""
-echo ""
-echo ""
 sudo wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.50.tar.gz
 sudo tar xvfz bcm2835-1.50.tar.gz
 cd bcm2835-1.50
@@ -60,30 +52,18 @@ sudo make
 sudo make install
 cd /home/pi
 echo ""
-echo ""
-echo ""
 echo "install homebridge plugins"
 sudo npm install -g homebridge-raspberrypi-temperature
 sudo npm install -g homebridge-gpio-device --unsafe-perm
 sudo npm install -g homebridge-dht-sensor --unsafe-perm
 sudo npm install -g homebridge-tradfri-plugin
-
 echo -e 'PATH="/opt/node/lib/node_modules/homebridge/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
-echo ""
-echo ""
-echo ""
-echo "iam ready..enjoy!! :) "
-echo ""
-echo ""
 echo ""
 echo "node version:"
 node -v
 echo ""
-echo ""
-echo ""
 echo "npm version:"
 npm -v
 echo ""
-echo ""
-echo ""
+echo "i'm ready.. enjoy! :) "
