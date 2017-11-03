35b220b001f5769d7d7c8f82b3acaf1c8f526685
Fekete András Demeter <andrei.dumitru.fechete@gmail.com>
2017-11-03 22:26:43 +0100

    .

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ sudo npm install -g homebridge-dht-sensor --unsafe-perm
 sudo npm install -g homebridge-tradfri-plugin
 echo -e 'PATH="/opt/node/lib/node_modules/homebridge/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
-echo -e 'PATH="/opt/node/lib/node_modules/homebridge/bin:$PATH"' >> ~/.profilesudo
+echo -e 'PATH="/opt/node/lib/node_modules/homebridge/bin:$PATH"' >> ~/.profile
 # kernel revert to 4.4.50!
 sudo rpi-update 52241088c1da59a359110d39c1875cda56496764  
 echo ""
